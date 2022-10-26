99b7e51cbf1402e4fcd25b7b876d1e1525539132
shantelraya <parischax@gmail.com>
2022-10-26 18:07:05 +0300

    done assignments

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+# Your code goes here
+class Anagram 
+
+    attr_accessor :customer
+
+    def initialize(word)
+        @customer = word 
+    end 
+
+    def match(array)
+        array.select {|x| x.split("").sort == @customer.split("").sort}
+    end
+
+end 
+
